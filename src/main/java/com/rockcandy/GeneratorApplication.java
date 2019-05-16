945c11f772ac46a34c0971ff7c685421bf87bb68
tangzedong <1064698801@qq.com>
2019-05-16 12:46:29 +0800

    springboot启动回档

---
+++ src/main/java/com/rockcandy/GeneratorApplication.java
@@ -0,0 +1,18 @@
+package com.rockcandy;
+
+import com.rockcandy.common.utils.GenUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tangzedong.programmer@gmail.com
+ * @apiNote generator sever start
+ * @since 2019/4/9 15:42
+ */
+@SpringBootApplication
+public class GeneratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorApplication.class, args);
+        GenUtils.generatorCode();
+    }
+}
